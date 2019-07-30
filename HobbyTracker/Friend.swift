23112ed8c3d30bd44f74c6a517a03ab780d19eb6
John Kouris <jdkouris@gmail.com>
2019-07-29 18:48:50 -0700

    Created the FriendsTableViewController and conformed to the TableViewDataSource protocol Created the Friend struct

---
+++ HobbyTracker/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  HobbyTracker
+//
+//  Created by John Kouris on 7/29/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct Friend {
+    let name: String
+    let hometown: String
+    var hobbies: [String]
+}
---
+++ HobbyTracker/FriendsTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  FriendsTableViewController.swift
+//  HobbyTracker
+//
+//  Created by John Kouris on 7/29/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var friends = [Friend]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension FriendsTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
